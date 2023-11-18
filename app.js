2f0f11783d1e74a6ff7565db1f1f115994b2ef00
Alex Greenberg <acgreenberg2@gmail.com>
2023-11-18 09:18:04 -0500

    testing the loop for prompts and pushing into object

--- app.js
+++ app.js
@@ -1,41 +1,77 @@
 console.log("hey")
 
 const words = {
-    number: 4,
-    adjective: "tall",
-    pluralNoun: "houses",
-    adverb: "strongly",
-    anotherAdjective: "nice",
-    isComplete: false
+    number: "",
+    adjective: "",
+    pluralNoun: "",
+    adverb: "",
+    anotherAdjective: "",
 }
 
 // we want a prompt to continue to appear asking for each piece of info until all pieces of info are complete
 // im thinking a While Loop for prompts and until the last is complete
 // isComplete added to the object - start it at false, we'll have a function update it to true once all prompts are delivered
 
-const finishInput = () => {
-    words.isComplete = true
-}
 
 // while loop to create prompts while isComplete is false
 
-while(words.isComplete === false) {
-    const NumberInput = prompt("give me a number")    // test that we create a variable for the number input
-    words.number = Number(NumberInput)                        // put it in the number key of the words object and make sure its a number          
-    finishInput()                                     // run the finishInput sequence   
-}
+// while(words.number === false) {
+//     const NumberInput = prompt("give me a number")    // test that we create a variable for the number input
+//     words.number = Number(NumberInput)                        // put it in the number key of the words object and make sure its a number          
+//                                        // run the finishInput sequence   
+// }
+// while(words.adjective === false) {
+//     const newAdjective =
+// }
 
-console.log(words
-    )  // logs a number for the number input
+// console.log(words
+      // logs a number for the number input
 
 // within the while loop, we'll create pushes for all prompts
 
-while(words.isComplete === false) {
-    const NumberInput = prompt("give me a number")    
-    words.number = Number(NumberInput)
-    const adjectiveInput = prompt("give me an adjective")     // run the adjective input right after the number input
-    words.adjective = adjectiveInput
-    finishInput()                                               //finish sequence to test           
+// while(words.isComplete === false) {
+//     const NumberInput = prompt("give me a number")    
+//     words.number = Number(NumberInput)
+//     while(words.number ) {
+//         const adjectiveInput = prompt("give me an adjective")     // run the adjective input right after the number input
+//         words.adjective = adjectiveInput
+//     } 
+
+//     finishInput()                                               //finish sequence to test           
+// }
+
+// console.log(words)
+
+// thats not looping to the next prompt
+
+// maybe we can create prompts that deliver through a for of loop
+// create an array of the questions
+
+const questions = ["give me a number", "and an adjective?", "and a plural noun", "now an adverb", "and finally one more adjective"]
+const answers = []
+
+// for of loop to ask prompts and print an array of answers
+
+for(let i of questions) {
+    const userInput = prompt(i)
+    answers.push(userInput)
 }
 
+console.log(answers)
+
+// function to put the answers to the array into the object
+
+const addInputObj = () => {
+    words.number = Number(answers[0])
+    words.adjective = answers[1]
+    words.adverb = answers[2]
+}
+addInputObj()
+
 console.log(words)
+
+
+// // while loops, one by one
+// while(words.number === false) {
+//     const numberInput = 
+// }
